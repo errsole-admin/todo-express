d82d6d8add2233d464bf652d9c21a4e60d01ff4b
Venkateswarlu Ganji <venki@errsole.com>
2023-10-10 14:38:26 +0530

    build: change subdomain

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/tasks', tasksRouter);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
