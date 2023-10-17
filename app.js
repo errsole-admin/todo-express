1e23e652f0a566d55ef75404f3b850fa3d01b94d
Venkateswarlu Ganji <venki@errsole.com>
2023-10-17 13:15:19 +0530

    build: install the morgan package

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 
 const tasksRouter = require('./routes/tasks');
 
 const app = express();
+
+app.use(morgan('combined'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -14,7 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/tasks', tasksRouter);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8082;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
